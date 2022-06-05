11bdd261b705522535a1cfe93a666b5e239d67a4
travisback <travisback@icloud.com>
2022-06-04 23:34:38 -0700

    Initial Commit

--- Swift_Day001_BandNameGenerator/main.swift
+++ Swift_Day001_BandNameGenerator/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Band Name Generator (v1.0)\n")
+
+print("What city did you grow up in? ")
+let city = readLine()
+
+print("What is your pet's name? ")
+let pet = readLine()
+
+print()
+
+print("Your band name is: " + city! + " " + pet!)
 
